e031c43638ebe3c855f5a773365d330ba25aa7de
PANTORY <pantory@gmail.com>
2021-09-17 21:54:18 +0300

    feat: add gallery section

---
+++ museum/src/components/gallery/gallery.js
@@ -0,0 +1,27 @@
+export const createGallery = () => {
+  const galleryContainer = document.querySelector('.gallery__wrapper');
+  const galleryImages = ['assets/images/gallery/gallery1.jpg', 'assets/images/gallery/gallery2.jpg', 'assets/images/gallery/gallery3.jpg',
+                         'assets/images/gallery/gallery4.jpg', 'assets/images/gallery/gallery5.jpg', 'assets/images/gallery/gallery6.jpg',
+                         'assets/images/gallery/gallery7.jpg', 'assets/images/gallery/gallery8.jpg', 'assets/images/gallery/gallery9.jpg',
+                         'assets/images/gallery/gallery10.jpg', 'assets/images/gallery/gallery11.jpg', 'assets/images/gallery/gallery12.jpg',
+                         'assets/images/gallery/gallery13.jpg', 'assets/images/gallery/gallery14.jpg', 'assets/images/gallery/gallery15.jpg'];
+
+  const shuffleArray = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  };
+  shuffleArray(galleryImages);
+
+  galleryImages.map((item) => {
+    let img = document.createElement('img');
+
+    galleryContainer.append(img);
+    img.classList.add('gallery__img');
+    img.src = item;
+    img.alt = `gallery${item.match(/\d+/)}`;
+  });
+};
+createGallery();
--- museum/src/scripts/index.js
+++ museum/src/scripts/index.js
@@ -3,3 +3,4 @@ import '../styles/style.scss';
 
 //- *** COMPONENTS ***
 import '../components/video-player/video-player.js';
+import { createGallery } from '../components/gallery/gallery.js';
