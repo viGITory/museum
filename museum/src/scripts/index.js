fe1ed458039cb36306a24b33db4f794259df8b40
PANTORY <pantory@gmail.com>
2021-09-27 01:06:36 +0300

    feat: add self-rate to console

--- museum/src/scripts/index.js
+++ museum/src/scripts/index.js
@@ -7,3 +7,6 @@ import '../components/compare-slider/compare-slider.js';
 import createGallery from '../components/gallery/gallery.js';
 import showModal from '../components/booking-modal/booking-modal.js';
 import addRipple from '../components/booking-overview/booking-overview.js';
+
+// *** SELF-RATE ***
+import './self-rate.js';
