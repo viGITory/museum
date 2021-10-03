70376da0cb0e54b7a32f611d1aad7ffafcee7005
PANTORY <pantory@gmail.com>
2021-10-03 23:38:37 +0300

    refactor: add adaptive images/minor styles fixes

--- museum/src/components/gallery/gallery.js
+++ museum/src/components/gallery/gallery.js
@@ -1,10 +1,112 @@
 const createGallery = () => {
   const galleryContainer = document.querySelector('.gallery__wrapper');
-  const galleryImages = ['assets/images/gallery/gallery1.jpg', 'assets/images/gallery/gallery2.jpg', 'assets/images/gallery/gallery3.jpg',
-                         'assets/images/gallery/gallery4.jpg', 'assets/images/gallery/gallery5.jpg', 'assets/images/gallery/gallery6.jpg',
-                         'assets/images/gallery/gallery7.jpg', 'assets/images/gallery/gallery8.jpg', 'assets/images/gallery/gallery9.jpg',
-                         'assets/images/gallery/gallery10.jpg', 'assets/images/gallery/gallery11.jpg', 'assets/images/gallery/gallery12.jpg',
-                         'assets/images/gallery/gallery13.jpg', 'assets/images/gallery/gallery14.jpg', 'assets/images/gallery/gallery15.jpg'];
+  const galleryImages = [
+    {
+      webp: 'assets/images/gallery/gallery1.webp',
+      jpg: 'assets/images/gallery/gallery1.jpg',
+      width: 464,
+      height: 464,
+      alt: `Psyche Revived by Cupid's Kiss`
+    },
+    {
+      webp: 'assets/images/gallery/gallery2.webp',
+      jpg: 'assets/images/gallery/gallery2.jpg',
+      width: 464,
+      height: 580,
+      alt: 'The Diana of Versailles or Artemis, Goddess of the Hunt'
+    },
+    {
+      webp: 'assets/images/gallery/gallery3.webp',
+      jpg: 'assets/images/gallery/gallery3.jpg',
+      width: 464,
+      height: 580,
+      alt: 'The Dying Slave - Michelangelo'
+    },
+    {
+      webp: 'assets/images/gallery/gallery4.webp',
+      jpg: 'assets/images/gallery/gallery4.jpg',
+      width: 464,
+      height: 464,
+      alt: 'Winged Victory of Samothrace'
+    },
+    {
+      webp: 'assets/images/gallery/gallery5.webp',
+      jpg: 'assets/images/gallery/gallery5.jpg',
+      width: 464,
+      height: 580,
+      alt: 'The Venus de Milo'
+    },
+    {
+      webp: 'assets/images/gallery/gallery6.webp',
+      jpg: 'assets/images/gallery/gallery6.jpg',
+      width: 464,
+      height: 580,
+      alt: 'The Virgin and Child with Saint Anne (Leonardo)'
+    },
+    {
+      webp: 'assets/images/gallery/gallery7.webp',
+      jpg: 'assets/images/gallery/gallery7.jpg',
+      width: 464,
+      height: 580,
+      alt: 'The Mona Lisa'
+    },
+    {
+      webp: 'assets/images/gallery/gallery8.webp',
+      jpg: 'assets/images/gallery/gallery8.jpg',
+      width: 464,
+      height: 580,
+      alt: ''
+    },
+    {
+      webp: 'assets/images/gallery/gallery9.webp',
+      jpg: 'assets/images/gallery/gallery9.jpg',
+      width: 464,
+      height: 580,
+      alt: ''
+    },
+    {
+      webp: 'assets/images/gallery/gallery10.webp',
+      jpg: 'assets/images/gallery/gallery10.jpg',
+      width: 464,
+      height: 464,
+      alt: 'Cour Visconti roof of the Department of Islamic Arts'
+    },
+    {
+      webp: 'assets/images/gallery/gallery11.webp',
+      jpg: 'assets/images/gallery/galler11.jpg',
+      width: 464,
+      height: 464,
+      alt: 'Liberty Leading the People'
+    },
+    {
+      webp: 'assets/images/gallery/gallery12.webp',
+      jpg: 'assets/images/gallery/gallery12.jpg',
+      width: 464,
+      height: 348,
+      alt: 'The Cour Marly'
+    },
+    {
+      webp: 'assets/images/gallery/gallery13.webp',
+      jpg: 'assets/images/gallery/gallery13.jpg',
+      width: 464,
+      height: 348,
+      alt: 'Sleeping Hermaphrodite'
+    },
+    {
+      webp: 'assets/images/gallery/gallery14.webp',
+      jpg: 'assets/images/gallery/gallery14.jpg',
+      width: 464,
+      height: 580,
+      alt: 'La Belle Ferronnière by Leonardo da Vinci'
+    },
+    {
+      webp: 'assets/images/gallery/gallery15.webp',
+      jpg: 'assets/images/gallery/gallery15.jpg',
+      width: 464,
+      height: 464,
+      alt: ''
+    }
+  ];
 
   const shuffleArray = (arr) => {
     for (let i = arr.length - 1; i > 0; i--) {
@@ -16,12 +118,23 @@ const createGallery = () => {
   shuffleArray(galleryImages);
 
   galleryImages.map((item) => {
+    let picture = document.createElement('picture');
+    let source = document.createElement('source');
     let img = document.createElement('img');
 
-    galleryContainer.append(img);
+    galleryContainer.append(picture);
+    picture.append(source, img);
+
+    picture.classList.add('gallery__picture');
     img.classList.add('gallery__img');
-    img.src = item;
-    img.alt = `gallery${item.match(/\d+/)}`;
+
+    source.srcset = item.webp;
+    img.src = item.jpg;
+
+    img.width = item.width;
+    img.height = item.height;
+
+    img.alt = item.alt;
   });
 };
 createGallery();
