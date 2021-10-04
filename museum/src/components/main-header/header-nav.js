b6ad32bb76d18a2d4d89bcb029bd46fefede58a9
PANTORY <pantory@gmail.com>
2021-10-04 20:30:01 +0300

    fix: open/close header nav action

--- museum/src/components/main-header/header-nav.js
+++ museum/src/components/main-header/header-nav.js
@@ -7,3 +7,21 @@ burgerBtn.addEventListener('click', () => {
   burgerBtn.classList.toggle('js-open-burger');
   welcomeText.classList.toggle('js-hide-welcome');
 });
+
+const hideNav = () => {
+  headerNav.classList.remove('js-show-nav');
+  burgerBtn.classList.remove('js-open-burger');
+  welcomeText.classList.remove('js-hide-welcome');
+};
+
+window.addEventListener('resize', () => {
+  if (document.body.clientWidth >= 1920) hideNav();
+});
+
+document.addEventListener('click', (event) => {
+  if (!(event.target.classList.contains('burger-btn')) &&
+      !(event.target === headerNav) &&
+      !(event.target.classList.contains('main-header__menu'))) {
+    hideNav();
+  }
+});
