c0f58275b601fa38ffda3c6f2374f4df707d50e4
PANTORY <pantory@gmail.com>
2022-01-05 14:56:45 +0300

    refactor: add stylelint webpack plugin

--- museum/webpack.config.js
+++ museum/webpack.config.js
@@ -1,55 +1,68 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const StylelintPlugin = require('stylelint-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const ImageminWebpWebpackPlugin= require("imagemin-webp-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
 
-const pages = ['index.pug', 'tours1.html', 'tours2.html', 'tours3.html', 'tours4.html', 'tours5.html', 'tours6.html', 'tours7.html'];
+const pages = [
+  'index.pug',
+  'tours1.html',
+  'tours2.html',
+  'tours3.html',
+  'tours4.html',
+  'tours5.html',
+  'tours6.html',
+  'tours7.html',
+];
 
 const createHtmlWebpackPlugin = pages.map((item) => {
   if (item === 'index.pug') {
     return new HtmlWebpackPlugin({
       template: `src/pages/${item}`,
       favicon: './src/favicon.ico',
-      filename: 'index.html'
+      filename: 'index.html',
     });
   } else if (item.startsWith('tours')) {
     return new HtmlWebpackPlugin({
       template: `src/pages/tours/${item}`,
       favicon: './src/favicon.ico',
-      filename: `tours/${item}`
+      filename: `tours/${item}`,
     });
   } else {
     return new HtmlWebpackPlugin({
       template: `src/pages/${item}`,
       favicon: './src/favicon.ico',
-      filename: item
+      filename: item,
     });
   }
 });
 
-const devServer = (isDev) => !isDev ? {} : {
-  devServer: {
-    open: true,
-    port: 8080,
-    contentBase: "./src"
-  },
-};
+const devServer = (isDev) =>
+  !isDev
+    ? {}
+    : {
+        devServer: {
+          open: true,
+          port: 8080,
+          contentBase: './src',
+        },
+      };
 
 module.exports = ({ development }) => ({
   mode: development ? 'development' : 'production',
   devtool: development ? 'inline-source-map' : false,
   entry: {
-    index: './src/scripts/index.js'
+    index: './src/scripts/index.js',
   },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'assets/images/[name][ext]'
+    assetModuleFilename: 'assets/images/[name][ext]',
   },
   optimization: {
-    minimize: false
+    minimize: false,
   },
   module: {
     rules: [
@@ -57,50 +70,57 @@ module.exports = ({ development }) => ({
         test: /\.svg$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'assets/svg/[name][ext]'
+          filename: 'assets/svg/[name][ext]',
         },
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/i,
-        type: 'asset/resource'
+        type: 'asset/resource',
       },
       {
         test: /\.(woff(2)?|eot|ttf|otf)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'assets/fonts/[name][ext]'
+          filename: 'assets/fonts/[name][ext]',
         },
       },
       {
         test: /\.pug$/,
-        loader: 'pug-loader'
+        loader: 'pug-loader',
       },
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: 'style.css' }),
     ...createHtmlWebpackPlugin,
+    new StylelintPlugin({
+      configFile: '.stylelintrc.json',
+      fix: true,
+      files: '**/*.scss',
+      syntax: 'scss',
+      failOnError: false,
+    }),
     new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: "src/assets",
-          to: "assets"
-        }
+          from: 'src/assets',
+          to: 'assets',
+        },
       ],
     }),
-    new ImageminWebpWebpackPlugin()
+    new ImageminWebpWebpackPlugin(),
   ],
   resolve: {
     extensions: ['.js'],
   },
-  ...devServer(development)
+  ...devServer(development),
 });
